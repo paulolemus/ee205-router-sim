10294ae082087894f137b80ee74a823176f46414
jessiejames <jlblanke@hawaii.edu>
2017-04-01 17:43:59 +0000

    added more to packets, trying to integrate them with the queue template

--- Packet.h
+++ Packet.h
@@ -8,9 +8,35 @@
  * as discrete packets that are passed through the router.
  */
 
+#include <ctime>
+
+
+
 struct Packet {
     long int arrival_time;
     int process_time;
     int dest;
+    int i; 
+    const int arrival_number =i;
 
+    long int set_arrival_time(int range){
+        std::srand(std::time(NULL)); //for random numbers
+        return arrival_time = rand() % range; 
+    }
+    
+    int set_process_time(int range){
+        std::srand(std::time(NULL)); //for random numbers
+        return process_time = rand() % range; 
+    }
+    
+    int set_arrival_number(void){
+        while (i){
+            i++;
+            return i;  
+        }
+    }
+    
+    const int get_arrival_number(void){
+        return arrival_number; 
+    }
 };
--- jess_test.cpp
+++ jess_test.cpp
@@ -16,13 +16,20 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
-
 #include "Packet.h"
+
+//queue stuff
+#include <stdexcept> //had to add this for runtime and other random things that wouldn't compile
+#include <exception>
+#include "Queue.h"
+
 using namespace std;
 
 void configSetter (void);
 
 int main() {
+	
+
     
     // configSetter();
 	string filename = "config.txt";
@@ -35,63 +42,63 @@ int main() {
 
 	//read from file
 	cout << "reading from input file\n";
-	string q1a;
-	string q1b;
-	string q1p;
-	string q1e;
-	string Q1;
-	string q2p;
-	string q2e;
-	string Q2;
-	string q3p;
-	string q3e;
-	string Q3;
-	
-
+	string q1a, q1b, q1p, q1e, Q1, q2p, q2e, Q2, q3p, q3e, Q3;
+	//pulling in variables from file 
 	infile >> q1a; 
-	cout << "q1a = " << q1a << endl;
 	infile >> q1b; 
-	cout << "q1b = " << q1b << endl;
 	infile >> q1p; 
-	cout << "q1p = " << q1p << endl;
 	infile >> q1e; 
-	cout << "q1e = " << q1e << endl;
 	infile >> Q1; 
-	cout << "Q1 = " << Q1 << endl << endl;
 	
 	infile >> q2p; 
-	cout << "q2p = " << q2p << endl;
 	infile >> q2e; 
-	cout << "q2e = " << q2e << endl;
 	infile >> Q2; 
-	cout << "Q2 = " << Q2 << endl << endl;
 	
 	infile >> q3p; 
-	cout << "q3p = " << q3p << endl;
 	infile >> q3e; 
-	cout << "q3e = " << q3e << endl;
 	infile >> Q3; 
+
+	cout << "q1a = " << q1a << endl;
+	cout << "q1b = " << q1b << endl;
+	cout << "q1p = " << q1p << endl;
+	cout << "q1e = " << q1e << endl;
+	cout << "Q1 = " << Q1 << endl << endl;
+	
+	cout << "q2p = " << q2p << endl;
+	cout << "q2e = " << q2e << endl;
+	cout << "Q2 = " << Q2 << endl << endl;
+	
+	cout << "q3p = " << q3p << endl;
+	cout << "q3e = " << q3e << endl;
 	cout << "Q3 = " << Q3 << endl;
 // 		cout << "\n This should parse out each set of consecutive numbers \n\n\n";
 	
 	infile.close();
 	
 	Packet *current = new Packet; 
+
 	int timer = 1;
     int arrivals = 20; 
-    int conv = stoi(q1b, NULL, 10);
+    int conva = stoi(q1a, NULL, 10);
+    int convb = stoi(q1b, NULL, 10);
     // std::string::size_type sz;   // alias of size_t
 	// int conv = std::stoi (q1b,&sz);
 
     std::srand(std::time(NULL)); //for random numbers
-    int lambda1 = rand() % conv +1; 
+    int lambda1 = convb-conva; 
     cout << "lambda1: " << lambda1 << endl; 
+    
+    current->set_arrival_time(lambda1); 
+    cout << current->arrival_time << endl; 
+    // current->set_arrival_number(0);
+    cout << current->arrival_number << endl; 
+    
+    
     for (timer; arrivals !=0; timer++){
         cout << "arrival count: " << arrivals << "\t\t";
         arrivals --; 
         
 		//enque in Q1
-	
 		
 		
         //after processing send to Q2 or Q3
@@ -103,6 +110,34 @@ int main() {
         	cout << "Sending packet to Q3" << endl;
         }
     }
+    
+    ///////////////////////////////////////////
+    //             TEST CASES                //
+    ///////////////////////////////////////////
+	std::cout << std::endl;
+    // queue objects from both implementations
+    ee::array::Queue<Packet> arrayQueue(5);
+
+    // Verify size and capacity functions are working
+    std::cout << "arrayQueue capacity: " << arrayQueue.capacity() << std::endl;
+    std::cout << "arrayQueue size: "     << arrayQueue.size()     << std::endl;
+
+    // Completely fill queue
+    // arrayQueue.enqueue(0);
+    // arrayQueue.enqueue(2);
+    // arrayQueue.enqueue(3);
+    // arrayQueue.enqueue(4);
+    // arrayQueue.enqueue(5);
+    // arrayQueue.enqueue(6);
+
+    // Cap and size should both be full
+    std::cout << "arrayQueue capacity: " << arrayQueue.capacity() << std::endl;
+    std::cout << "arrayQueue size: "     << arrayQueue.size()     << std::endl;
+    std::cout << "Is arrayQueue full? "  << arrayQueue.isFull()   << std::endl;
+
+    // test print function, prints from base to top of queue
+    // arrayQueue.print();
+    std::cout << std::endl;
     return 0;
 }
 
