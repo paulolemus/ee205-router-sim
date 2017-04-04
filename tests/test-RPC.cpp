24c15ab556270cd4d935c96bcb99f9164ec02e71
jessiejames <jlblanke@hawaii.edu>
2017-04-04 09:07:13 +0000

    able to tokenize by unwanted items (spaces, dashes, newline, etc)

--- tests/test-RPC.cpp
+++ tests/test-RPC.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>
 #include <sstream>
+#include <string.h>
+#include <stdio.h>
 #include "../Stack.h"
 #include "../RPC.h"
 
@@ -13,18 +15,36 @@ using namespace std;
 
 int main(){
     //prompt user
-    cout << "RPC: input a line in Reverse Polish Notation form:"; 
+    cout << "RPC: input a line in Reverse Polish Notation form:" << endl; 
     
     // get input
     string s; 
     // cin >> s;  
     getline (cin, s);
-    cout << "--- received: " << s << endl; 
+    cout << "---first received line: " << s << endl; 
+    
+    char *duplicate = strdup(s.c_str()); 
+    char *token1;
+    char delim[] = " ,.;'-?:!\t\n";
+    token1 = strtok(duplicate, delim);
+    
+
+  while( token1 != NULL ) 
+  {
+    cout << "in while loop, token is: " << token1 << endl;
+    token1 = strtok(NULL, delim);
+  }
+    free(duplicate);
     
     
     //check if its a #
+        // if true - push
+        
+        
     
     //check if its an op
+        // if true - pop 2 numbers
+        // perform op
     
     //error checking
     
